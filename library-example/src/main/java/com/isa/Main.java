3cf7c170ecefcb7541357c325cb580255b027259
szy-puszcz <szymon.puszcz@rsqtechnologies.com>
2023-04-22 11:16:41 +0200

    library exercises

---
+++ library-example/src/main/java/com/isa/Main.java
@@ -0,0 +1,33 @@
+package com.isa;
+
+public class Main {
+    public static void main(String[] args) {
+        String text = "Witaj XYZ!";
+
+        /**
+         * Zadanie 1
+         * Wypisz w konsoli zmienną text w odwróconej kolejności.
+         * Znajdź metodę służącą do odwracania kolejności w klasie StringUtils
+         */
+
+
+        /**
+         * Zadanie 2
+         * Wypisz w konsoli zmienną text, tak aby zawierała tylko małe listery.
+         * Wykonaj to zadanie na dwa sposoby, najpierw używając metody z klasy String na obiekcie text,
+         * a następnie używając klasy StringUtils jak w poprzednim zadaniu.
+         */
+
+        /**
+         * Zadanie 3
+         * Wylosuj dwie liczby (int) korzystając z klasy RandomUtils.
+         * Przypisz je do zmiennych i wypisz w konsoli
+         */
+
+        /**
+         * Zadanie 4
+         * Wypisz w konsoli mniejszą z liczb wylosowanych w poprzednim zadaniu.
+         * Do znalezienia mniejszej liczby skorzystaj z klasy NumberUtils.
+         */
+    }
+}
